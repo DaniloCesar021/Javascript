b33ed0eefd0e5ce6753c233760690626a3d7cab2
DaniloCesar021 <129341712+DaniloCesar021@users.noreply.github.com>
2024-05-15 23:13:16 -0300

    j
    j

--- Aula16/funcao01.js
+++ Aula16/funcao01.js
@@ -1,4 +1,4 @@
-function parimp(n) { 
+ function parimp(n) { 
     if (n % 2 == 0) {
         return 'par'
     } else {
@@ -6,4 +6,17 @@ function parimp(n) {
     }
 }
 var res = parimp(19322)
-console.log(res)
+console.log(res) 
+
+
+/*
+function vel(v){
+    if (v >= 80){
+        return 'cuidado alta velocidade'
+    }
+    else {
+        return 'limite permitido'
+    }
+}
+let r = vel(100)
+console.log(r) */
