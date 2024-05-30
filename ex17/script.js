355e12cf0507f9a8361b8f76a84d91a16d48140d
DaniloCesar021 <129341712+DaniloCesar021@users.noreply.github.com>
2024-05-29 21:57:34 -0300

    d0.1
    d0.1

--- ex17/script.js
+++ ex17/script.js
@@ -1,28 +1,48 @@
+//Globais
+var add = document.getElementById('num')
+var res = document.getElementById('res')
+var tab = document.getElementById('sel')
 
+//
+var ln = []
+
+//
+
+
+//
 function adicionar() {
-    let add = document.getElementById('num')
-    let res = document.getElementById('res')
-    let tab = document.getElementById('sel')
     if (add.value > 100 || add.value <= 0) {
         alert('deu juca')
 
 
     } else {
-        let ln = [ Number(add.value)]
+        //let ln = []
+        let num = Number(document.getElementById('num').value)
+        ln = [num]
         let item = document.createElement('option')
-        tab.innerHTML += ''
-        item.text += `Valor: ${ln}`
+        item.text += `Valor: ${num}`
         tab.appendChild(item)
-        
-        
+        /* let ln = [Number(add.value)]
+         let item = document.createElement('option')
+         tab.innerHTML += ''
+         item.text += `Valor: ${n}`
+         tab.appendChild(item)
+         tab.sort() */
+
+
+
+
     }
+
 }
-
-
-
-
-
+//////////////////////////////////////////////////////////
 
 function finalizar() {
+     
+ 
+    res.innerHTML(item)
+    res.innerHTML = `Ao todo temos ${tab.length} numeros`
+    
+
 
 }
--- sandbox/js.js
+++ sandbox/js.js
@@ -1,21 +1,3 @@
-function poi(){
-    let ip1 = document.getElementById('ip1')
-    let res = document.getElementById('res')
-    parouimp = Number(ip1.value)
-    if (parouimp % 2 == 0) {
-        res.innerHTML = 'Par'
-    } else {
-        res.innerHTML= 'impar'
-    }
 
-}
-function poi(x){
-    let res = document.getElementById('res')
-    let parouimp = Number(x.value)
-    if (parouimp % 2 == 0) {
-        res.innerHTML = 'Par'
-    } else {
-        res.innerHTML= 'impar'
-    }
 
-}
+
