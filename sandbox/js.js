871edd8c2d83e839507da81b5c6eea43043c0cfd
DaniloCesar021 <129341712+DaniloCesar021@users.noreply.github.com>
2024-06-01 10:56:55 -0300

    biscoito0.2
    biscoito0.2

--- sandbox/js.js
+++ sandbox/js.js
@@ -1,9 +1,9 @@
 var n = 0
-  function clicar(){
-     n++
-    let res = document.getElementById('res')
-    res.innerHTML  = (` + ${n} biscoitos`)
-    
+function clicar() {
+  n++
+  let res = document.getElementById('res')
+  res.innerHTML = (` + ${n} biscoitos`)
+
 }
 
 
