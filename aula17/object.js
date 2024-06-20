c0c15752158dc8aa4f89616a5fb32419cf9357c5
DaniloCesar021 <129341712+DaniloCesar021@users.noreply.github.com>
2024-06-19 21:20:10 -0300

    eita
    eita

--- aula17/object.js
+++ aula17/object.js
@@ -1,11 +1,22 @@
-let amigo = {nome: `carlos`,sexo: 'masculino',peso: 90,
+/*let amigo = {nome: `carlos`,sexo: 'masculino',peso: 90,
     engordar(p = 0) {
         console.log('engordou')
         this.peso += p
     }
 }
 amigo.engordar(3)
-console.log(`${amigo.nome} pesa ${amigo.peso} kilos`)
+console.log(`${amigo.nome} pesa ${amigo.peso} kilos`)*/
+
+
+let pai = {
+    nome: 'luiz', sexo: 'masculino', peso: 70,
+    peso1(p = 0) {
+        this.peso += p
+    }
+}
+
+pai.peso1(10)
+console.log(pai.peso)
 
 
 
